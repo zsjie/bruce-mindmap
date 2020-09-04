093a876a6ea5f19492bfd3295691f61d3dec9d9b
zhushijie <zhushijie@jd.com>
2020-09-04 15:55:00 +0800

    fix: lodash not included in bundle file

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
-import typescript from '@rollup/plugin-typescript';
+import Babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript'
 
 export default {
   input: 'src/index.ts',
@@ -6,9 +9,22 @@ export default {
     file: '_dev/assets/brucemap.js',
     exports: 'auto',
     format: 'umd',
-    name: 'BruceMap'
+    name: 'BruceMap',
+    globals: {
+      lodash: 'lodash'
+    },
   },
   plugins: [
+    nodeResolve(),
+    commonjs(),
+    Babel({
+      include: ['src/**'],
+      exclude: /\**node_modules\**/,
+      extensions: ['.js', '.ts'],
+      babelrc: false,
+      babelHelpers: 'runtime',
+      skipPreflightCheck: true,
+    }),
     typescript(),
   ],
 }
--- src/core.ts
+++ src/core.ts
@@ -1,5 +1,57 @@
+import { entries, map } from 'lodash'
 export default class BruceMap {
+  private container: HTMLDivElement
+
+  constructor(containerId) {
+    const container = document.querySelector(containerId)
+    if (!container) {
+      throw new Error('Board Container Not Found')
+    }
+    this.container = container
+    this.initBoard()
+  }
+
+  private initBoard() {
+    this.container.setAttribute('style', formatStyleStr({
+      position: 'relative',
+      overflow: 'auto',
+    }))
+
+    const svg = document.createElement('svg')
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+    svg.setAttribute('version', '1.1')
+    svg.setAttribute('style', formatStyleStr({
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      display: 'block',
+      width: '100%',
+      height: '100%',
+      'min-width': '3200px',
+      'min-height': '1800px',
+      'background-image': 'none',
+    }))
+    const rect = document.createElement('rect')
+    rect.setAttribute('width', '200')
+    rect.setAttribute('height', '40')
+    rect.setAttribute('fill', '#c70415')
+    const text = document.createElement('text')
+    text.setAttribute('x', '100')
+    text.setAttribute('y', '20')
+    text.setAttribute('fill', '#ffffff')
+    text.setAttribute('text-anchor', 'middle')
+    text.innerHTML = 'Root Topic'
+    svg.appendChild(rect)
+    svg.appendChild(text)
+
+    this.container.appendChild(svg)
+  }
+
   public greet() {
     console.log('hello world!')
   }
+}
+
+function formatStyleStr(styles = {}): string {
+  return map(entries(styles), entry => `${entry[0]}: ${entry[1]}`).join('; ')
 }
